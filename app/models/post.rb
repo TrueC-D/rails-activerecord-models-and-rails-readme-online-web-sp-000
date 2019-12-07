770ac4b8923b70f74e6743ae17116cb6cb6b36b5
Alethea Cary <aletheacary@gmail.com>
2019-12-07 02:56:02 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,3 @@
 class Post
-  ActiveRecord::Migration
-  def change
-    create_table :posts do |t| 
-      t.string :title
-      t.text :description
-      t.timestamps null: false
-    end
-  end
+  
 end
